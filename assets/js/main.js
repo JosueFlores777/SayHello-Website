647478ada79175f854a960c220e91ca487b32e52
bryan.flores <bryanjosueflores24@gmail.com>
2024-05-06 10:47:37 -0600

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,126 @@
+
+var boxes = [
+  {
+    typeWork: 'Marketing Campaign ',
+    image: 'https://images.unsplash.com/photo-1712675670807-f925abe035b3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    titleModal: 'Bebemundo',
+    Subtitle: 'Lorem ipsum dolor sit amet, consectetur  aliquam sed quam.',
+    cardText: ' Bebemundo is the biggest baby items store in El Salvador. We partnered with them for their summer campaign, aiming to capture the essence of the season with eye-catching images that resonate with their audience.',
+
+    imgcard1: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+    imgcard2: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+    imgcard3: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+    imgcard4: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+
+    testiModal: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere doloremque nihil voluptatibus perferendis sed, nulla, odit vel minima dolores iure exercitationem debitis ut alias vitae in dolore eos praesentium quis.',
+    imgTestim: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+    nameTest: 'Name 1',
+  },
+  {
+    typeWork: 'Website Development',
+    image: '../assets/img/Portfolio2024/Equinoccio/equinoccio-project-showcase 01.png',
+    titleModal: 'Hotel Equinoccio',
+    Subtitle: 'Lorem ipsum dolor sit amet, consectetur  aliquam sed quam.',
+    cardText: 'Equinoccio is a hotel situated in Lake Coatepeque, a popular tourist spot in El Salvador. Our client wanted to revamp their website to mirror the aesthetics and cleanliness portrayed in their social media. Together, we worked on UX/UI design and website development to achieve their desired look and feel.',
+
+    video: '../assets/img/Portfolio2024/videos/Hotel Equinoccio Showcase_1080p.mp4',
+    imgcard1: '../assets/img/Portfolio2024/Equinoccio/equinoccio-project-showcase 02.png',
+    imgcard2: '../assets/img/Portfolio2024/Equinoccio/equinoccio-project-showcase 03.png',
+    imgcard3: '../assets/img/Portfolio2024/Equinoccio/equinoccio-project-showcase 04.png',
+    imgcard4: '../assets/img/Portfolio2024/Equinoccio/equinoccio-project-showcase 05.png',
+
+
+    testiModal: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere doloremque nihil voluptatibus perferendis sed, nulla, odit vel minima dolores iure exercitationem debitis ut alias vitae in dolore eos praesentium quis.',
+    imgTestim: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+    nameTest: 'Name 1',
+  }, {
+    typeWork: 'Marketing Campaign',
+    image: 'https://images.unsplash.com/photo-1712675670807-f925abe035b3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    titleModal: 'Teatro Luis Poma',
+    Subtitle: 'Lorem ipsum dolor sit amet, consectetur  aliquam sed quam.',
+    cardText: 'We collaborated with Teatro Luis Poma for their 20th anniversary celebration of continuous theater seasons. For this special occasion, we created the "Teatromorphosis" campaign together.',
+    video: '../assets/img/Portfolio2024/videos/Teatro Luis Poma20th Anniversary.mp4',
+    imgcard1: '../assets/img/Portfolio2024/TeatroPoma/PORTADA-facebook-Naara.png',
+    imgcard2: '../assets/img/Portfolio2024/TeatroPoma/Mini campana-tlp logo12.jpg',
+    imgcard3: '../assets/img/Portfolio2024/TeatroPoma/portada web_acto 1_2022.png',
+    imgcard4: '../assets/img/Portfolio2024/TeatroPoma/Mini campana-tlp logo12.jpg',
+
+    testiModal: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere doloremque nihil voluptatibus perferendis sed, nulla, odit vel minima dolores iure exercitationem debitis ut alias vitae in dolore eos praesentium quis.',
+    imgTestim: 'https://images.pexels.com/photos/9482920/pexels-photo-9482920.jpeg?auto=compress&cs=tinysrgb&w=660&h=650&dpr=1',
+    nameTest: 'Name 1',
+  }
+];
+function selectBox(index) {
+  document.getElementById("fullscreen-background").style.backgroundColor = "#343333a7";
+  document.getElementById("fullscreen-background").style.zIndex = 10;
+  var selectedBox = boxes[index - 1];
+  document.getElementById("scrollerWork").style.display = "block";
+  document.getElementById("workcardModal").textContent = selectedBox.typeWork;
+  document.getElementById("titleModal").textContent = selectedBox.titleModal;
+ /* document.getElementById("titleWork").textContent = selectedBox.Subtitle;*/
+  document.getElementById("cardText").textContent = selectedBox.cardText;
+
+  document.getElementById("video").src = selectedBox.video;
+  document.getElementById("imgcardPri").src = selectedBox.image;
+  document.getElementById("imgcard1").src = selectedBox.imgcard1;
+  document.getElementById("imgcard2").src = selectedBox.imgcard2;
+  document.getElementById("imgcard3").src = selectedBox.imgcard3;
+  document.getElementById("imgcard4").src = selectedBox.imgcard4;
+
+  document.getElementById("testomioModa").textContent = selectedBox.testiModal;
+  document.getElementById("avatarTesti").src = selectedBox.imgTestim;
+  document.getElementById("nameTesti").textContent = selectedBox.nameTest;
+  var video = document.getElementById('video');
+  video.muted = false;
+}
+
+function clearSelection() {
+  var video = document.getElementById('video');
+  if (video) {
+      video.muted = true;
+  }
+  document.getElementById("scrollerWork").scrollTop = 0;
+  document.getElementById("scrollerWork").style.display = "none";
+  document.getElementById("fullscreen-background").style.zIndex = -6;
+  document.getElementById("fullscreen-background").style.backgroundColor = "black";
+
+}
+
+document.getElementById("scrollerWork").addEventListener("scroll", function () {
+  var scrollPosition = this.scrollTop;
+  if (window.innerWidth < 478) {
+    paddingValue = scrollPosition > 0 ? 0 : 30;
+    var paddi = "padding-left 0.9s ease, padding-right 0.9s ease";;
+  } else {
+    paddingValue = scrollPosition > 0 ? 0 : 60;
+    var paddi = "padding-left 0.4s ease, padding-right 0.4s ease";;
+  }
+  this.style.transition = paddi;
+  this.style.padding = paddingValue + "px";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const scrollers = document.querySelectorAll(".scrollerHome");
 
 // Observador de intersección para mostrar el contenido cuando se hace scroll
@@ -269,3 +392,4 @@ if (isMobile) {
     scrollerInner.style.animationPlayState = "running";
   });
 }
+
